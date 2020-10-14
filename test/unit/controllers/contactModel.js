6b92121ffe34d86812d3456d18e4aaee0f1bd864
kapil0309 <kapil.yadav3@tcs.com>
2020-10-14 17:17:58 +0530

    app added

---
+++ test/unit/controllers/contactModel.js
@@ -0,0 +1,21 @@
+// contactModel.js
+var mongoose = require('mongoose');
+// Setup schema
+var contactSchema = mongoose.Schema({
+    name: {
+        type: String,
+        required: true
+    },
+    
+    gender: String,
+    phone: String,
+    ailments: String,
+    medicines: String,
+    dob: String
+    
+});
+// Export Contact model
+var Contact = module.exports = mongoose.model('contact', contactSchema);
+module.exports.get = function (callback, limit) {
+    Contact.find(callback).limit(limit);
+}
